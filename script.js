4885ed44882f13f378682ab242c9a11ee655e9e1
Muneer Girnari <muneeragirnari@gmail.com>
2025-02-26 19:36:46 +0530

    modified html and script.js file

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 //your JS code here. If required.
+let btn = document.getElementById("btn");
+let age = document.getElementById("age");
+let name = document.getElementById("name");
+
+btn.addEventListener("click",listener);
+
+function listener(){
+    if(age.value === "" || name.value === ""){
+        alert("Please enter valid details.");
+    }
+
+    if(age.value>=18){
+        setTimeout(() => {
+            new Promise((resolve) => {
+                resolve(alert("Welcome, . You can vote."))
+            })
+        },4000)
+    }
+
+    if(age.value<18){
+        setTimeout(() => {
+            new Promise((resolve) =>{
+                resolve(alert("Oh sorry . You aren't old enough."))
+            })
+        },4000);
+    }
+}
